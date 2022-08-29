dddfb41590caa66bcefd8cf653770e0caf3f95b6
Parth <work.parthm257@gmail.com>
2022-08-28 23:00:15 -0700

    removeCors

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,6 @@ module.exports = {
     new ModuleFederationPlugin({
         library: { type: "module" },
 
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-          "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        },
-
         // For remotes (please adjust)
         name: "loginDemo",
         filename: "remoteEntry.js",
